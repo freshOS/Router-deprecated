2e9630986637f2911e32a6748d63e5c10ea93fe4
S4cha <sachadso@gmail.com>
2019-12-23 15:17:04 +0100

    Updates Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,11 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Router"
+    name: "Router",
+    platforms: [.iOS(.v8)],
+    products: [.library(name: "Router", targets: ["Router"])],
+    targets: [.target(name: "Router")]
 )
